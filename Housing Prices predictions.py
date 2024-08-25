cc340e4b8f2504012c3c0c4ccec6344b83c61a46
Karabo Tigedi <154095813+Karabo20Tigedi@users.noreply.github.com>
2024-08-25 19:20:56 +0200

    Updated the file by adding pipelines and columns transfomers to make the code less messier and more readable

--- Housing Prices predictions.py
+++ Housing Prices predictions.py
@@ -1,77 +1,54 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import OneHotencoder
 
-# Read the data
-X_full = pd.read_csv("C:\Users\nelly\OneDrive - University of Cape Town\Data Science\PERSONAL PROJECTS\Machine Learning\Housing Prices predictions\Data\train.csv", index_col='Id')
-X_test_full = pd.read_csv("C:\Users\nelly\OneDrive - University of Cape Town\Data Science\PERSONAL PROJECTS\Machine Learning\Housing Prices predictions\Data\test.csv"", index_col='Id')
 
-# Remove rows with missing target, separate target from predictors
-X_full.dropna(axis=0, subset=['SalePrice'], inplace=True)
-y = X_full.SalePrice
-X_full.drop(['SalePrice'], axis=1, inplace=True)
+# READING DATA STAGE
+X_train_full = pd.read_csv("C:\Users\nelly\OneDrive - University of Cape Town\Data Science\PERSONAL PROJECTS\Machine Learning\Housing Prices predictions\Data\train.csv", index_col = "Id")
+X_test_full = pd.read_csv("C:\Users\nelly\OneDrive - University of Cape Town\Data Science\PERSONAL PROJECTS\Machine Learning\Housing Prices predictions\Data\test.csv", index_col = "Id")
 
-# To keep things simple, we'll use only numerical predictors
-X = X_full.select_dtypes(exclude=['object'])
-X_test = X_test_full.select_dtypes(exclude=['object'])
+#CLEANING DATA STAGE
+X_train_full.dropna(axis = 0, subset = ['SalePrice'], inplace = True)
+y = X_train_full['SalePrice']
+X_train_full.drop(['SalePrice'], axis = 1, inplace = True)
 
-# Break off validation set from training data
-X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=0)
+X_train_o, X_vaid_o, y_train, y_valid = train_test_split(X_train_full,y, train_size = 0.8, test_size = 0.2, random_state =0)
 
-# Function for comparing different approaches for models
-def score_dataset(X_train, X_valid, y_train, y_valid):
-    model = RandomForestRegressor(n_estimators=100, random_state=0)
-    model.fit(X_train, y_train)
-    preds = model.predict(X_valid)
-    return mean_absolute_error(y_valid, preds)
+num_cols = [col for col in X_train_o.columns if X_train_o[col].dtype in ['float64','int64']]
+cat_cols = [col for col in X_train_o.columns if X_train_o[col].dtype == ['object'] and X_train_o[col].nunique < 10]
 
-#Checking for number of missing values in each column of training data
-missing_val_count_by_column = (X_train.isnull().sum())
-print(missing_val_count_by_column[missing_val_count_by_column >0])
+my_cols = num_cols +cat_cols
+X_train = X_train_o[my_cols].copy()
+X_valid = X_valid_o[my_cols].copy()
+X_test = X_test_full[my_cols].copy()
 
-#APPROACH ONE: Dropping columns with missing values
-reduced_X_train = X_train.drop(['LotFrontage','MasVnrArea','GarageYrBlt'], axis =1)
-reduced_X_valid = X_valid.drop(['LotFrontage','MasVnrArea','GarageYrBlt'], axis = 1)
 
-print("MAE (Drop columns with missing values):")
-print(score_dataset(reduced_X_train, reduced_X_valid, y_train, y_valid))
+#PREPROCESSING STAGE
 
-#APPROACH TWO: Imputation
+num_transformer = SimpleImputer(strategy = 'mean')
+cat_transformer = Pipeline(steps = [('Impute', SimpleImputer(strategy = 'most_frequent'),('OH', OneHotEncoder(handle_unknown = 'ignore')])
 
-imputer = SimpleImputer()
-imputed_X_train = pd.DataFrame(imputer.fit_transform(X_train))
-imputed_X_valid = pd.DataFrame(imputer.transform(X_valid))
+transformer = ColumnTransformer(transformers = [('num', num_transformer, num_cols),('cat', cat_transformer, cat_cols)])
 
-#imputation removed column names; put them back
-imputed_X_train.columns = X_train.columns
-imputed_X_valid.columns = X_valid.columns
+#MODEL DEFINITION, FITTING, AND PREDICTION
+model = RandomForestRegressor(n_estimators = 100, random_state =0)
 
-print("MAE (Imputation):")
-print(score_dataset(imputed_X_train, imputed_X_valid, y_train, y_valid))
+my_pipeline = Pipeline( steps = [('transformer', transformer),('model',model)])
 
-#Based on the data for, the Mean Absolute Error for the second approach is smaller so we train the model
-# with the second approach
+my_pipeline.fit(X_train, y_train)
 
-# Preprocessed training and validation features
-final_X_train = imputed_X_train.copy()
-final_X_valid = imputed_X_valid.copy()
+predictoions = my_pipeline.predict(X_valid)
+print('MAE: ' + mean_absolute_error(y_vaid, predictions))
 
-# Define and fit model
-model = RandomForestRegressor(n_estimators=100, random_state=0)
-model.fit(final_X_train, y_train)
 
-# Get validation predictions and MAE
-preds_valid = model.predict(final_X_valid)
-print("MAE (Your approach):")
-print(mean_absolute_error(y_valid, preds_valid))
 
-# Preprocess test data
-final_imputer = SimpleImputer()
-final_X_test = pd.DataFrame(final_imputer.fit_transform(X_test))
 
-# Get test predictions
-preds_test = model.predict(final_X_test)
+
+
 
 
